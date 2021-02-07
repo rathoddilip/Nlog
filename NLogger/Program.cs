0411757bc732f7e2636fe8241c3f4a69ed48c47e
diliprathod32 <diliprathod32@gmail.com>
2021-02-07 14:47:29 +0530

    [Dilip] ExceptionLogToOutputNlogFile

---
+++ NLogger/Program.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+using System;
+
+namespace NLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int zero = 0;
+                int result = 10 / zero;
+            }
+            catch (Exception ex)
+            {
+                Logger logger = LogManager.GetCurrentClassLogger();
+                logger.Error(ex);
+            }           
+            Console.WriteLine("Nlog file");
+            Console.ReadLine();
+        }
+    }
+}
